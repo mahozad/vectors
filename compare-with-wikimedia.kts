98eb0853598e5504a9f147f0531537d4c3a62024
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-12-22 22:25:49 +0330

    WIP - Implement the script

--- compare-with-wikimedia.kts
+++ compare-with-wikimedia.kts
@@ -1,18 +1,27 @@
 #!kotlinc -script -jvm-target 11
 
+import java.io.File
 import java.io.InputStreamReader
 import java.net.URL
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.stream.Collectors
 
-val ignored = listOf(
-        Path.of("./subpixel-arrangement/"),
-)
+val WIKIMEDIA_BASE_URL = "https://upload.wikimedia.org/wikipedia/commons/"
+val ignored = listOf(Path.of("./subpixel-arrangement/"))
 
-val readmes = Files.find(Path.of("."), 5, { path: Path, _ ->
-    path.toFile().name == "README.md"
-}).filter {
+val readmes = getAllReadmeFiles()
+for (readme in readmes) {
+    val vector = findVectorFile(readme)
+    val imagePage = extractWikimediaLink(readme)
+    val imageUrl = extractRawUrl(imagePage)
+    val areSynced = compare(vector, imageUrl)
+    println("File://$vector: $areSynced ")
+}
+
+fun getAllReadmeFiles() = Files.find(Path.of("."), 5,
+        { path, _ -> path.toFile().name == "README.md" }
+).filter {
     val content = Files.readString(it)
     var index = content.indexOf("Wikimedia page", 0, true)
     return@filter index >= 0
@@ -20,26 +29,29 @@ val readmes = Files.find(Path.of("."), 5, { path: Path, _ ->
     !ignored.contains(it.parent)
 }.collect(Collectors.toList())
 
-for (readme in readmes) {
-    val vector = sequenceOf(*Files.list(readme.parent).collect(Collectors.toList()).toTypedArray())
-            .findLast { it.toFile().name.matches(Regex("\\d+-.*\\.svg")) }
+fun findVectorFile(readme: Path) = readme.parent.toFile().listFiles().findLast { it.name.matches(Regex("\\d+-.*\\.svg")) }
 
+fun extractWikimediaLink(readme: Path?): URL {
     val content = Files.readString(readme)
     var index = content.indexOf("Wikimedia page", 0, true)
     val startParIndex = content.indexOf("(", index)
     val endParIndex = content.indexOf(")", startParIndex)
     val link = content.substring((startParIndex + 1)..(endParIndex - 1))
+    return URL(link)
+}
 
-    val input = URL(link).openStream()
+fun extractRawUrl(link: URL): URL {
+    val input = link.openStream()
     val page = InputStreamReader(input).readText()
-    val url = "https://upload.wikimedia.org/wikipedia/commons/" + page.substringAfter("<a href=\"https://upload.wikimedia.org/wikipedia/commons/").substringBefore("\"")
+    val url = WIKIMEDIA_BASE_URL + page.substringAfter("<a href=\"$WIKIMEDIA_BASE_URL").substringBefore("\"")
+    return URL(url)
+}
 
-    // val length = URL(url).openConnection().getHeaderField("content-Length").toLong()
-    val length = URL(url).openStream().readAllBytes().size.toLong()
-    print("File://$vector ")
-    if (length == vector!!.toFile().length()) {
-        println("The same")
+fun compare(offline: File?, online: URL): Boolean {
+    // if (offline!!.length() != online.openConnection().getHeaderField("content-Length").toLong()) return false
+    if (offline!!.readBytes().contentEquals(online.openStream().readAllBytes())) {
+        return true
     } else {
-        println("Not the same")
+        return false
     }
 }
