04b85b8357a991285ef0d122c701d994291c14ce
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2020-12-24 22:08:37 +0330

    Simplify some statements

--- compare-with-wikimedia.kts
+++ compare-with-wikimedia.kts
@@ -1,7 +1,6 @@
 #!kotlinc -script -jvm-target 11
 
 import java.io.File
-import java.io.InputStreamReader
 import java.net.URL
 import java.nio.file.Files
 import java.nio.file.Path
@@ -37,8 +36,7 @@ fun findVectorFile(readme: File) = readme.parentFile.listFiles().findLast {
 fun extractWikiLink(readme: File) = URL(readme.readText().substringBetween("$LINK_LABEL(", ")"))
 
 fun loadWikiVector(link: URL): File {
-    val input = link.openStream()
-    val page = InputStreamReader(input).readText()
+    val page = link.openStream().reader().readText()
     val path = page.substringBetween("<a href=\"$WIKI_BASE_URL", "\"")
     val url = URL("$WIKI_BASE_URL$path")
     return createTempFile().apply { writeBytes(url.readBytes()) }
@@ -53,7 +51,7 @@ fun createTempFile() = Files.createTempFile(null, null).toFile()
 fun String.substringBetween(s1: String, s2: String) = substringAfter(s1).substringBefore(s2)
 
 fun prettyPrint(vector: File, areIdentical: Boolean) {
-    val name = vector.parent.removePrefix(".\\").removePrefix("./")
+    val name = vector.parentFile.normalize()
     val label = if (areIdentical) "${ANSI_CYAN}identical" else "${ANSI_RED}different"
     println(String.format("├%-32s┼%9s┤", "─", "─").replace(' ', '─'))
     println(String.format("│%-32s│$label$ANSI_RESET│", name))
